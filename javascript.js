fa99bd1707a4ee66f539b909ec3daf3ee3229522
Marcos Manoel <marcosmss1304@gmail.com>
2019-07-14 11:25:27 -0300

    menu bug

--- javascript.js
+++ javascript.js
@@ -57,3 +57,36 @@ fetch("https://jsonplaceholder.typicode.com/posts/")
   });
 
 //Está feio pra caramba o código, irei fazer uns commits refatorando.
+
+//searchbox
+
+// function myFunction() {
+//   // Declare variables
+//   var input, filter, ul, li, a, i, txtValue;
+//   input = document.getElementById("myInput");
+//   filter = input.value.toUpperCase();
+//   ul = document.getElementById("list-users");
+//   li = ul.getElementsByTagName("li");
+
+//   // Loop through all list items, and hide those who don't match the search query
+//   for (i = 0; i < li.length; i++) {
+//     a = li[i].getElementsByTagName("a")[0];
+//     txtValue = a.textContent || a.innerText;
+//     if (txtValue.toUpperCase().indexOf(filter) > -1) {
+//       li[i].style.display = "";
+//     } else {
+//       li[i].style.display = "none";
+//     }
+//   }
+// }
+
+// menu;
+
+function myMenu() {
+  var x = document.getElementById("menuTop");
+  if (x.className === "menu") {
+    x.className += "responsive";
+  } else {
+    x.className = "menu";
+  }
+}
